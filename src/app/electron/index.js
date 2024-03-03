e4a0e5bbe3f3909ff730b6930810ff4f7ae4f8ae
Joel <brolgedu@gmail.com>
2024-03-03 13:59:23 -0800

    Changde window resultion to 80% of display; auto hide menu

--- src/app/electron/index.js
+++ src/app/electron/index.js
@@ -1,12 +1,22 @@
-const { BrowserWindow, app } = require('electron');
+const { BrowserWindow, app, screen } = require('electron');
 require('electron-reload')(__dirname);
 
 const createWindow = () => {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  const percentage = 0.8;
+  const windowWidth = Math.floor(width * percentage);
+  const windowHeight = Math.floor(height * percentage);
+
   const win = new BrowserWindow({
-    width: 800, height: 600, minWidth: 800, minHeight: 600, // titleBarStyle: "hidden",
+    width: windowWidth,
+    height: windowHeight,
+    minWidth: 800,
+    minHeight: 600,
+    titleBarStyle: 'default',
+    autoHideMenuBar: true,
   });
   win.loadFile('./app/react/build/index.html').then(() => console.log('File loaded'));
-  win.webContents.openDevTools();
 };
 
 app.whenReady().then(createWindow);
--- src/app/react/src/components/Table/index.jsx
+++
@@ -1,7 +0,0 @@
-import { memo } from 'react';
-
-function Table({ headers, columns, rows, children, ...rest }) {
-  return <div></div>;
-}
-
-export default memo(Table);
