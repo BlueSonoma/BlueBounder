b5c5b7046c51e47e329cdd357bb6bdc6f4aa7637
Joel <brolgedu@gmail.com>
2024-03-03 14:11:19 -0800

    Abstracted Viewport into its own component

--- src/app/react/src/components/ViewportMetricsBar/index.jsx
+++ src/app/react/src/components/ViewportMetricsBar/index.jsx
@@ -1,11 +1,11 @@
 import { getZoomPercentage } from '../Canvas/utils';
 import Button from '../additional-components/buttons/Button';
 import IconFullScreen from '../../resources/icons/full-screen.png';
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
 import useViewport from '../../hooks/useViewport';
 
-function ViewportMetricsBar({ className, zoom, minZoom, maxZoom, onFitView, children }) {
-  const { mousePosition } = useViewport();
+function ViewportMetricsBar({ className, onFitView, children }) {
+  const { mousePosition, maxZoom, minZoom, zoom } = useViewport();
 
   //TODO: Implement pixel measurement as such:
   //  1 pixel = 10µm
@@ -17,25 +17,25 @@ function ViewportMetricsBar({ className, zoom, minZoom, maxZoom, onFitView, chil
   //  so be sure to pick an appropriate length
 
   return (<div className={'viewport-footer ' + className}>
-      <div style={{ paddingLeft: '10px' }}>|______50µm______|</div>
-      <div
-        style={{
-          display: 'flex', flexDirection: 'row', paddingRight: '5px', alignItems: 'center',
-        }}
-      >
-        <div style={{ paddingRight: '10px' }}>
-          X={mousePosition.x.toFixed(2)} Y={mousePosition.y.toFixed(2)} Zoom=
-          {`${getZoomPercentage(zoom, minZoom, maxZoom)}%`}
-        </div>
-        <Button
-          onClick={onFitView}
-          style={{ marginTop: '7px', border: 'none' }}
-          imageStyle={{ width: '20px', height: '20px' }}
-          imageUrl={IconFullScreen}
-        />
-        {children}
+    <div style={{ paddingLeft: '10px' }}>|______50µm______|</div>
+    <div
+      style={{
+        display: 'flex', flexDirection: 'row', paddingRight: '5px', alignItems: 'center',
+      }}
+    >
+      <div style={{ paddingRight: '10px' }}>
+        X={mousePosition.x.toFixed(2)} Y={mousePosition.y.toFixed(2)} Zoom=
+        {`${getZoomPercentage(zoom, minZoom, maxZoom)}%`}
       </div>
-    </div>);
+      <Button
+        onClick={onFitView}
+        style={{ marginTop: '7px', border: 'none' }}
+        imageStyle={{ width: '20px', height: '20px' }}
+        imageUrl={IconFullScreen}
+      />
+      {children}
+    </div>
+  </div>);
 }
 
-export default memo(ViewportMetricsBar);
+export default ViewportMetricsBar;
