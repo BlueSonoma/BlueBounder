eae2af45c6c367b5583a3ecb4187bdbf0e8d7c85
Joel <brolgedu@gmail.com>
2024-03-15 22:06:43 -0700

    Changed `/home` url to `/app` and component from `Canvas` to `App`

--- src/app/react/src/components/screens/RoutesComponent.jsx
+++ src/app/react/src/components/screens/RoutesComponent.jsx
@@ -4,16 +4,16 @@ import Start from './Start';
 import NewSession from './NewSession';
 import OldSessions from './OldSessions';
 import SessionInitializer from './SessionIntializer';
-import Canvas from '../Canvas';
+import App from '../App';
 
 export function RoutesComponent() {
   return (<Router>
-    <Routes>
-      <Route path='/' element={<Start />} />
-      <Route path='/newSession' element={<NewSession />} />
-      <Route path='/home' element={<Canvas />} />
-      <Route path='/oldSessions' element={<OldSessions />} />
-      <Route path='/initSession' element={<SessionInitializer />} />
-    </Routes>
-  </Router>);
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/newSession' element={<NewSession />} />
+        <Route path='/app' element={<App />} />
+        <Route path='/oldSessions' element={<OldSessions />} />
+        <Route path='/initSession' element={<SessionInitializer />} />
+      </Routes>
+    </Router>);
 }
