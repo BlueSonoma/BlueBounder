795c85ce2f3eeafe20820a0632d6b93c770e8f12
Joel <brolgedu@gmail.com>
2024-03-18 14:28:49 -0700

    Formatting

--- src/app/react/src/components/sidebars/SettingsSidebar/index.jsx
+++ src/app/react/src/components/sidebars/SettingsSidebar/index.jsx
@@ -3,10 +3,6 @@ import TabbedSidebar from '../TabbedSidebar';
 import { DockPanelPosition } from '../../../types/general';
 import LayersView from '../../views/ViewportTreeView';
 
-const components = {
-  label: 'Layers', component: <LayersView />,
-};
-
 function SettingsSidebar({ show, children, ...rest }) {
   return (<TabbedSidebar
     show={show}
