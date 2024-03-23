a73d821793dad323e90feb1d0d21a30aa4003e42
Joel <brolgedu@gmail.com>
2024-03-23 08:46:22 -0700

    Added `Frame` component to standardize look of panel containers

--- src/app/react/src/containers/Frame/index.jsx
+++ src/app/react/src/containers/Frame/index.jsx
@@ -3,7 +3,7 @@ import '../../styles/bounder.css';
 
 function Frame({ label, bodyProps, children, ...rest }) {
   return (<div className={'bounder__frame'}>
-    <div className={'header'} {...rest}>{label ?? ''}</div>
+    {label && <div className={'header'} {...rest}>{label}</div>}
     <div className={'body'} {...bodyProps}>
       {children}
     </div>
