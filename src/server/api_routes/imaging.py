385b6e195efa7ced97a2420096b281d2e1867255
Joel <brolgedu@gmail.com>
2024-03-24 16:33:02 -0700

    Added `exec_segmentation` api call to execute boundary segmentation

--- src/server/api_routes/imaging.py
+++ src/server/api_routes/imaging.py
@@ -1,8 +1,11 @@
+import os.path
+
 import flask
 from flask import Blueprint, jsonify
 from skimage import io
 
 from src.imaging.Magic import clean_chemistry
+from src.imaging.segmentation.utils import create_directory
 
 api = Blueprint('imaging', __name__)
 
@@ -14,6 +17,49 @@ def api__clean_chemistry():
     case = flask.request.form['case']
     threshold = flask.request.form['threshold']
     clean_chemistry()
+
+
+@api.route('/exec_segmentation', methods=['POST'])
+def api__exec_segmentation():
+    from src.imaging.segmentation.segmentation import segment_boundaries
+
+    chem_image_path = flask.request.form['chemImagePath']
+    scale = flask.request.form['scale']
+    sigma = flask.request.form['sigma']
+    min_size = flask.request.form['minSize']
+    outline_color = flask.request.form['outlineColor']
+    label = flask.request.form['label']
+    uniform_label = flask.request.form['uniformLabel']
+    overlay = flask.request.form['overlay']
+    band_image_path = flask.request.form['bandImagePath']
+    label_color = flask.request.form['labelColor']
+    label_opacity = flask.request.form['labelOpacity']
+    output_path = flask.request.form['outputPath']
+    output_filename = flask.request.form['outputFilename']
+
+    create_directory(output_path)
+
+    try:
+        chemImage = io.imread(chem_image_path)
+        bandImage = io.imread(band_image_path)
+        segmented = segment_boundaries(chemImage,
+                                       scale=int(scale),
+                                       sigma=float(sigma),
+                                       min_size=int(min_size),
+                                       outline_color=outline_color,
+                                       label=bool(label),
+                                       label_uniform=bool(uniform_label),
+                                       overlay=bool(overlay),
+                                       overlay_image=bandImage,
+                                       label_color=label_color,
+                                       label_opacity=float(label_opacity)
+                                       )
+        path = os.path.join(output_path, output_filename + '.png')
+        io.imsave(path, segmented)
+        return jsonify(path, 200)
+    except Exception as e:
+        print(str(e))
+        return str(e), 500
 
 
 @api.route('/sample_pixel', methods=['GET'])
