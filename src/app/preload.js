1d303e5a4dfa7534a7b9ff1c787de913db126ab8
Joel <brolgedu@gmail.com>
2024-03-24 16:30:47 -0700

    Added ipc channel communication between React and Electron

--- src/app/preload.js
+++ src/app/preload.js
@@ -1,12 +1,47 @@
-const { ipcRenderer } = require('electron');
-const { channels } = require('../react/src/shared/constants');
+const { contextBridge, ipcRenderer } = require('electron');
+const { channels } = require('./react/src/shared/constants');
 
-process.once('loaded', () => {
-  window.addEventListener('message', event => {
-    console.log(event.data.type);
-    if (event.data.type === channels.OpenDirectoryDialog) {
-      ipcRenderer.send(channels.OpenDirectoryDialog);
+/**Place any functions or variables to be accessed outside of React
+@example
+ // App.jsx
+ import { electron } = window
+ export function App() {
+    useEffect(() => {
+      // Attach a listener to be notified of ipcRenderer messages
+      window.addEventListener('message', (event) => {
+        if (event.data.type === channels.SelectDirectoryDialog) {
+          console.log(event.data.path);
+        }
+      });
+      return () => {
+        window.removeEventListener('message', (event) => {
+          if (event.data.type === channels.SelectDirectoryDialog) {
+            console.log(event.data.path);
+          }
+        });
+      };
+    }, []);
+    ...
+    function onClick() {
+      // Call the function
+      electron.openDirectoryDialog();
     }
-  });
+*/
+
+contextBridge.exposeInMainWorld('electron', {
+  openDirectoryDialog: () => ipcRenderer.send(channels.OpenDirectoryDialog),
 });
+
+// Register any 'on' events to send back to React
+process.once('loaded', () => {
+  try {
+    ipcRenderer.on(channels.SelectDirectoryDialog, (event, path) => {
+      console.log(channels.SelectDirectoryDialog, path);
+      window.postMessage({ type: channels.SelectDirectoryDialog, path: path });
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
 
